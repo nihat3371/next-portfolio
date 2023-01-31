3ad540bfd8aaee27c16bd5f6d7c24a671c2bc48f
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2023-01-31 22:52:23 +0300

    srever

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   );
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const pageInfo: PageInfo = (await fetchPageInfo()) || null;
   const experiences: Experience[] = (await fetchExperiences()) || null;
   const skills: Skill[] = (await fetchSkills()) || null;
